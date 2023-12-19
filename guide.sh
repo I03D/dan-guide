9fa578f7e311bf3fc5d68771e54a275ac74a4820
I03D <98839825+I03D@users.noreply.github.com>
2023-12-20 02:23:07 +0700

    Update guide.sh

--- guide.sh
+++ guide.sh
@@ -232,7 +232,7 @@ case "$it" in
 	echo "sudo mv ~/DanOS/mouseless/ ~/.config/mouseless"
 	echo "mkdir ~/.config/mouseless/"
 	echo "mv ~/DanOS/mouseless/config.yaml ~/.config/mouseless/config.yaml"
-	echo "sudo mv DanOS/mouseless/mouseless /bin/mouseless"
+	echo "sudo mv ~/DanOS/mouseless/mouseless /bin/mouseless"
 	echo "sudo chmod 777 /usr/bin/mouseless"
 	
 	read
