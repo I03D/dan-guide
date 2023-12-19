c512733adfa994c206581074760417cb5d4bb308
I03D <98839825+I03D@users.noreply.github.com>
2023-12-19 22:04:56 +0700

    Update dan-guide.sh

--- dan-guide.sh
+++ dan-guide.sh
@@ -4,7 +4,8 @@ tmux rename-window 'Dan-Guide'
 tmux split-window -h
 tmux send-keys '# Для прокрутки используйте ctrl+B, затем - pgUp, pgDown. Чтобы закрыть эту подсказку, нажмите ctrl+B, X.' Enter
 tmux split-window -v
-tmux send-keys 'sh guide.sh' enter
+command="cd $(dirname "$0")"
+tmux send-keys "$(command); sudo sh guide.sh" enter
 tmux select-pane -t 1
 tmux -2 attach-session -t guide
 
