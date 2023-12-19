7ff35a5c4aac5c8a3167d137898dc835cde7a75f
I03D <98839825+I03D@users.noreply.github.com>
2023-12-19 22:40:29 +0700

    Update guide.sh

--- guide.sh
+++ guide.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+center() {
+  termwidth="$(tput cols)"
+  padding="$(printf '%0.1s' \ {1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+center "Something I want to print"
+
 clear
-echo ~ Dan-Guide ~
+
+center "~ Dan-Guide ~"
 echo Вы запустили неофициальный гид по установке ArchLinux. Для переключения между панелями использовать ctrl+b, затем - влево/вправо. Для продолжения нажимайте Enter.
 
 read
