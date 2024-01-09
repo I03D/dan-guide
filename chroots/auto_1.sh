fcba81c11cd36aabda623739f7fd01e66f12c56d
I03D <98839825+I03D@users.noreply.github.com>
2024-01-09 13:40:37 +0700

    bash config for root is /root/.profile

--- chroots/auto_1.sh
+++ chroots/auto_1.sh
@@ -97,5 +97,5 @@ pacman -S --noconfirm tmux
 echo ""
 
 echo "Запись следующего скрипта в .bashrc для автозапуска..."
-echo "setfont /usr/share/kbd/consolefonts/cyr-sun16.psfu.gz" >> /home/$username/.bashrc
-echo "sudo sh /opt/scripts/reboot_1a.sh" >> /etc/skel/.bashrc
+echo "setfont /usr/share/kbd/consolefonts/cyr-sun16.psfu.gz" >> /root/.profile
+echo "sudo sh /opt/scripts/reboot_1a.sh" >> /root/.profile
