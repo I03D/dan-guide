939558c3f4fff3c942552c0a984c1480debe23dc
I03D <98839825+I03D@users.noreply.github.com>
2024-01-22 12:27:15 +0700

    Update reboot_2.sh

--- chroots/reboot_2.sh
+++ chroots/reboot_2.sh
@@ -1,19 +1,19 @@
-
-# echo "sudo mouseless --config ~/.config/mouseless/config.yaml"
-
-echo "Устанавливаем автоматический запуск при старте системы:"
-sudo nvim /etc/systemd/system/mouseless.service
-sudo systemctl enable mouseless.service
-sudo systemctl start mouseless.service
-
-echo "Проверка:"
-sudo systemctl status mouseless.service
-echo "Должна быть пометка \"Active: active (running)\"."
-
-echo "Установка соответствующих настроек NeoVim и lf, если вы уже способны использовать Colemak-раскладку:"
-sudo cp DanOS/nvim/ .config/nvim -r
-sudo cp DanOS/nvim /root/.config/nvim -r
+echo "Тестирование mouseless:"
+echo "Проведите тест mouseless, нажав, например, alt+,"
 echo ""
-sudo cp DanOS/lf .config/lf/ -r
-sudo cp DanOS/lf /root/.config/lf/ -r
+echo "(Рекомендуется отключить интеграцию мыши при использовании виртуальной машины. Так будет виден подлинный курсор, не всегда совпадающий с вашим.)"
 
+echo ""
+
+echo "Если не получается, то, скорее всего, нужно загрузить модуль uinput и перезагрузить систему:"
+echo "echo \"uinput\" | sudo tee /etc/modules-load.d/uinput.conf && reboot"
+
+echo ""
+
+echo "Нажмите Enter, когда удостоверитесь, что mouseless работает для завершения установки."
+read
+
+clear
+
+echo "Установка завершена. Нажмите Enter для закрытия и удаления остаточных файлов."
+read
