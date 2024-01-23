7e1e1f0ab4579c7947f471d90d82eaf9faae4ece
I03D <98839825+I03D@users.noreply.github.com>
2024-01-23 18:38:32 +0700

    Update DarkTheme.sh

--- i3/DarkTheme.sh
+++ i3/DarkTheme.sh
@@ -1,7 +1,11 @@
 #/bin/bash
 
+export DYNAMIC_COLORS_COLORSCHEMES=/etc/dynamic-colors/colorschemes
+
+export XDG_CONFIG_HOME=/etc
+
 for filename in /dev/pts/*; do
 	echo $filename
-	/root/.dynamic-colors/bin/dynamic-colors switch 3024.dark.txt > $filename
+	/bin/dynamic-colors switch 3024.dark.txt > $filename
 done
 
