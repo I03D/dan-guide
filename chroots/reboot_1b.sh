bdf9ffe96a341887fcec2560b598d2ef4f697e30
I03D <98839825+I03D@users.noreply.github.com>
2024-01-23 17:46:08 +0700

    Update reboot_1b.sh

--- chroots/reboot_1b.sh
+++ chroots/reboot_1b.sh
@@ -120,7 +120,7 @@ echo "Скрытие папки добавлением точки в назва�
 
 echo ""
 
-mv dynamic-colors/ /root/.dynamic-colors/dynamic-colors
+mv dynamic-colors/ /bin/dynamic-colors # Или лучше /usr/bin/?
 echo "Изменение прав доступа к тёмной цветовой схеме для i3..."
 sudo chmod 100 /home/danil/.config/i3/DarkTheme.sh
 
