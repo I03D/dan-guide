5eda4556134bcd285b1ff01b2bb062d3eb56d15b
I03D <98839825+I03D@users.noreply.github.com>
2024-01-24 03:06:05 +0700

    Update LightTheme.sh

--- i3/LightTheme.sh
+++ i3/LightTheme.sh
@@ -5,7 +5,7 @@ export XDG_CONFIG_HOME=/etc
 
 for filename in /dev/pts/*; do
 	echo $filename
-	/bin/dynamic-colors switch 3024.dark.txt > $filename
+	/bin/dynamic-colors switch LightScheme > $filename
 done
 
 
