030b78a8d37b2c6ae82556f967a34a94fe2a1c83
I03D <98839825+I03D@users.noreply.github.com>
2024-01-24 00:46:05 +0700

    Update reboot_1b.sh

--- chroots/reboot_1b.sh
+++ chroots/reboot_1b.sh
@@ -122,12 +122,12 @@ echo ""
 
 mv dynamic-colors/bin/dynamic-colors /bin/dynamic-colors # Или лучше /usr/bin/?
 echo "Изменение прав доступа к тёмной цветовой схеме для i3..."
-sudo chmod 111 /home/danil/.config/i3/DarkTheme.sh
+sudo chmod 555 /home/danil/.config/i3/DarkTheme.sh
 
 echo ""
 
 echo "Изменение прав доступа к светлой цветовой схеме для i3..."
-sudo chmod 111 /home/danil/.config/i3/LightTheme.sh
+sudo chmod 555 /home/danil/.config/i3/LightTheme.sh
 echo ""
 echo "Установка gcc, требуемого для dynamic-colors..."
 sudo pacman -S --noconfirm gcc
