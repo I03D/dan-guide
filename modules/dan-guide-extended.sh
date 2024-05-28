064e7d431c4d89fea820bc28374a5c0b9dbe431a
I03D <98839825+I03D@users.noreply.github.com>
2024-05-29 04:49:07 +0700

    Update dan-guide-extended.sh

--- modules/dan-guide-extended.sh
+++ modules/dan-guide-extended.sh
@@ -1,1 +1,8 @@
-echo "test-from-module" > test.txt
+echo "Установка менеджера обоев fastfeh:"
+cd /opt/scripts/
+echo "Загрузка fastfeh..."
+git clone https://github.com/i03d/fastfeh
+echo "Изменение прав доступа к скрипту..."
+chmod -R 777 fastfeh/
+
+gap
