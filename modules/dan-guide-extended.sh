986b05a2d44b4a9ae3115189bd8e6fc39f8178cf
I03D <98839825+I03D@users.noreply.github.com>
2024-05-29 04:59:54 +0700

    Update dan-guide-extended.sh

--- modules/dan-guide-extended.sh
+++ modules/dan-guide-extended.sh
@@ -4,5 +4,8 @@ echo "Загрузка fastfeh..."
 git clone https://github.com/i03d/fastfeh
 echo "Изменение прав доступа к скрипту..."
 chmod -R 777 fastfeh/
+echo "Создание алиаса..."
+ln -s fastfeh/fastfeh.sh /bin/fastfeh
+cd ~
 
 gap
